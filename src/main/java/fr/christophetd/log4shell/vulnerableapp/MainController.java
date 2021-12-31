abc930aa0b36ff5924002cb4d0ec44e60fb41738
Kanat Nurzhanov <kanat_nurzhanov@epam.com>
2021-12-31 22:50:00 +0300

    Log4shell third issue: Add vulnerable code

--- src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
+++ src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
@@ -4,9 +4,12 @@ package fr.christophetd.log4shell.vulnerableapp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 
 @RestController
 public class MainController {
@@ -18,5 +21,10 @@ public class MainController {
         logger.info("Received a request for API version " + apiVersion);
         return "Hello, world!";
     }
-
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        ThreadContext.put("username", name);
+        logger.info("Greeting {}", name);
+        return "Hello " + name + "!";
+    }
 }
